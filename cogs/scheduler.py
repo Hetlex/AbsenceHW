fa5870e4c9f6a0b1f0f72e47ff00edded5289b7d
Hetlex <varrrapp2@gmail.com>
2025-09-23 17:53:59 +0300

    Должно работать

--- cogs/scheduler.py
+++ cogs/scheduler.py
@@ -20,19 +20,14 @@ class Scheduler(commands.Cog):
     async def check_time(self):
         now = datetime.datetime.now(self.moscow_tz)
 
-        
         if now.weekday() == 5 and now.hour == 22 and now.minute == 0:
             await self.send_reminder()
 
-        
         if now.weekday() == 6 and (now.hour == 5 or now.hour == 22) and now.minute == 0:
             await self.send_reminder()
 
-        
         if now.weekday() == 0 and now.hour == 5 and now.minute == 0:
             await self.send_reminder()
-
-    import random
 
     async def send_reminder(self):
         channel = self.bot.get_channel(CHANNEL_ID)
@@ -46,4 +41,8 @@ class Scheduler(commands.Cog):
                     f"{role.mention} Team up with your friends and try to reach tier 3 for the future of our guild!"
                 ]
                 await channel.send(random.choice(messages))
+
+async def setup(bot):
+    await bot.add_cog(Scheduler(bot))
+
 
