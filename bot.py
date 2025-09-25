d5c54e3e9aa48d6c44bf0ebb2d3373da2933b7c6
Hetlex <varrrapp2@gmail.com>
2025-09-25 19:40:17 +0300

    Откат

--- bot.py
+++ bot.py
@@ -2,13 +2,13 @@ import os
 import asyncio
 import discord
 from discord.ext import commands
-from dotenv import load_dotenv
+import logging
 
-load_dotenv() 
+logging.basicConfig(level=logging.INFO)
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 if not TOKEN:
-    raise ValueError("❌ Не найдена переменная окружения токен!")
+    raise ValueError("❌ Не найдена переменная окружения ток!")
 
 intents = discord.Intents.default()
 intents.message_content = True
